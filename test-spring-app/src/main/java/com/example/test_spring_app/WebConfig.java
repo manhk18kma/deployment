bf65318ec97ad93abc5c875ae0228ff4a5afb0ec
root <root@vmi2772038.contaboserver.net>
2025-08-24 20:46:15 +0200

    vps

--- test-spring-app/src/main/java/com/example/test_spring_app/WebConfig.java
+++ test-spring-app/src/main/java/com/example/test_spring_app/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(urlClient)
+                .allowedOrigins(urlClient.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
